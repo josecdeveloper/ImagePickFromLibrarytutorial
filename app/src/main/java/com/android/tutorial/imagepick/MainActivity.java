705fb70fe112da08ef6a126efc8781eadf50daa1
Jose Disla <Jose Disla>
2017-01-06 21:10:39 -0500

    Initial commit

---
+++ app/src/main/java/com/android/tutorial/imagepick/MainActivity.java
@@ -0,0 +1,61 @@
+package com.android.tutorial.imagepick;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadImagefromGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            //when an image is selected
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                //get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+            } else {
+                Toast.makeText(MainActivity.this, "You haven't picked an image", Toast.LENGTH_SHORT).show();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
